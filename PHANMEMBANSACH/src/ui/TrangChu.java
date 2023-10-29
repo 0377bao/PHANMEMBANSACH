69697d0454789d980efcd2b13d0b33c03d5eda71
Thuy Kieu <thuykieu130303@gmail.com>
2023-10-29 17:07:27 +0700

    test

--- PHANMEMBANSACH/src/ui/TrangChu.java
+++ PHANMEMBANSACH/src/ui/TrangChu.java
@@ -65,9 +65,9 @@ public class TrangChu extends JFrame {
 	 */
 	public TrangChu() {
 		this.setTitle("H1");
-		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1500, 800);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.getContentPane().setLayout(null);
