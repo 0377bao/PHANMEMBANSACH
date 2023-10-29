a58c74abac09111e855cecaa3867513ff6be1fd5
Bao <hbao27121@gmail.com>
2023-10-29 17:05:45 +0700

    update

--- PHANMEMBANSACH/src/ui/TrangChu.java
+++ PHANMEMBANSACH/src/ui/TrangChu.java
@@ -64,7 +64,7 @@ public class TrangChu extends JFrame {
 	 * Create the frame.
 	 */
 	public TrangChu() {
-		this.setTitle("H1");
+		this.setTitle("Push đi");
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1500, 800);
